b2abe8b10db83658a9ae5aac436de5d97529f331
DonValino <jk_valino@yahoo.com>
2016-02-01 11:15:01 +0000

    Added an abstract Vehicle class

---
+++ src/Vehicle.java
@@ -0,0 +1,16 @@
+/**
+ * Created by edoncva on 01/02/2016.
+ */
+public abstract class Vehicle {
+    //Instance Varriable
+    private int plateNumber;
+    private String colour;
+
+    //Constructor
+    public Vehicle (int plateNumberIn,String colourIn)
+    {
+        plateNumber = plateNumberIn;
+        colour = colourIn;
+    }
+
+}
